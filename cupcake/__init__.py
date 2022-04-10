7923f9e963e7cfce7650d99fb39918dba0327014
Sam <sam@shlarsen.com>
2022-04-10 05:49:00 -0400

    refactor: Remove unused solx version check

--- cupcake/__init__.py
+++ cupcake/__init__.py
@@ -1,4 +1,4 @@
-from solcx import install_solc, compile_standard, set_solc_version, get_solc_version
+from solcx import install_solc, compile_standard, set_solc_version
 from web3 import Web3, HTTPProvider, EthereumTesterProvider
 from web3.middleware import geth_poa_middleware
 from json import load, dump, dumps
